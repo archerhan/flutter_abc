0a87e8480d0dc7c49f0b6dd2872c229ebf49c3ee
archerhan <hanyancheng.tyut@gmail.com>
2019-04-16 16:45:18 +0800

    创建一个简单的 app

---
+++ simple_app_demo/lib/TabbarPageSecond.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TabbarPageSecond extends StatefulWidget {
+  @override
+  _TabbarPageSecondState createState() => _TabbarPageSecondState();
+}
+
+class _TabbarPageSecondState extends State<TabbarPageSecond> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ simple_app_demo/lib/TabbarPageThird.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TabbarPageThird extends StatefulWidget {
+  @override
+  _TabbarPageThirdState createState() => _TabbarPageThirdState();
+}
+
+class _TabbarPageThirdState extends State<TabbarPageThird> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
