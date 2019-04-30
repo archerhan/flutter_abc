2b37ef43733de896f9bbf32c6b061322eb398069
ArcherHan <archerhan1990@outlook.com>
2019-04-30 17:15:58 +0800

    配置网络模块

---
+++ music_player/lib/common/database.dart
@@ -0,0 +1,26 @@
+/**
+ * @author Archer
+ * @date 2019-04-30
+ * Description:
+ */
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
+
+export 'package:sembast/sembast.dart';
+export 'package:sembast/sembast_io.dart';
+
+Database _db;
+
+///Quiet application database
+Future<Database> getApplicationDatabase() async {
+	if (_db != null) {
+		return _db;
+	}
+	_db = await databaseFactoryIo.openDatabase(
+			join((await getTemporaryDirectory()).path, 'database', 'quiet.db'));
+	return _db;
+}
--- music_player/lib/page/home/personal_playlist.dart
+++ music_player/lib/page/home/personal_playlist.dart
@@ -1,6 +1,28 @@
 /**
  * @author Archer
  * @date 2019-04-29
- * Description:
+ * Description: 个人播放列表,首页
  */
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:music_player/widget/refresh_indicator.dart';
+
+class MainPlayListPage extends StatefulWidget {
+  @override
+  _MainPlayListPageState createState() => _MainPlayListPageState();
+}
+
+class _MainPlayListPageState extends State<MainPlayListPage> with AutomaticKeepAliveClientMixin {
+
+  GlobalKey<RefreshIndicatorState> _indicatorKey = GlobalKey();
+  GlobalKey<LoaderState> _loaderKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  // TODO: implement wantKeepAlive
+  bool get wantKeepAlive => null;
+}
