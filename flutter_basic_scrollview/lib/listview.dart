ca69a8f6ce7cb3e1aab69c0c9cabd5c4e9e70e7c
Archer <archerhan1990@outlook.com>
2019-06-09 20:46:22 +0800

    List view

---
+++ flutter_basic_scrollview/lib/listview.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class InfiniteListView extends StatefulWidget {
+  @override
+  _InfiniteListViewState createState() => _InfiniteListViewState();
+}
+
+class _InfiniteListViewState extends State<InfiniteListView> {
+  static const loadingTag = "##Loading##";
+  var _words = <String>[loadingTag];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _retrieveData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("List view demo"),
+      ),
+      body: ListView.separated(
+        itemCount: _words.length,
+        itemBuilder: (context, index) {
+          if (_words[index] == loadingTag) {
+            if (_words.length - 1 < 100) {
+              _retrieveData();
+              return Container(
+                padding: const EdgeInsets.all(16.0),
+                alignment: Alignment.center,
+                child: SizedBox(
+                  width: 24.0,
+                  height: 24.0,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 2.0,
+                  ),
+                ),
+              );
+            } else {
+              return Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "没有更多了",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            }
+          }
+          return ListTile(
+            title: Text(_words[index]),
+          );
+        },
+        separatorBuilder: (context, index) => Divider(
+              height: .0,
+            ),
+      ),
+    );
+  }
+
+  void _retrieveData() {
+    Future.delayed(Duration(seconds: 2)).then((e) {
+      _words.insertAll(
+          _words.length - 1,
+          //每次生成20个单词
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList());
+      setState(() {
+        //重新构建列表
+      });
+    });
+  }
+}
